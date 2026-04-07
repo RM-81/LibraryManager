d5f25d0a5d1c0164d98270de2424da96dfcc04a1
MAYEL <2405010@ugrad.cse.buet.ac.bd>
2026-04-07 18:26:21 +0600

    9th commit

--- src/main/java/com/example/librarymanager/HelloApplication.java
+++ src/main/java/com/example/librarymanager/HelloApplication.java
@@ -10,14 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // 1. Point this to your login fxml file
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log_out.fxml"));
 
-        // 2. Load the scene (936x558 matches your HBox pref sizes)
+
         Scene scene = new Scene(fxmlLoader.load());
 
         stage.setTitle("Bookverse - Login");
-        stage.setResizable(false); // Keeps your layout looking perfect
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
